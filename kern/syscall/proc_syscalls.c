06b98e21b0ac81acce2f2790450dcdb27d7777ef
Prasad Shivanna <prasadsjcecs@gmail.com>
2016-04-20 22:26:04 -0400

    fixed a memory leak in exec

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -342,6 +342,7 @@ sys_execv(const char *program, char **uargs){
 	//	kprintf("new_length: %d , pad_length: %d \n",arg_length, pad_length);
 		//arg = (char *)kmalloc(sizeof(arg_length));
 		arg = kstrdup(args[j]);
+		kfree(args[j]);
 		for (int i = 0; i < arg_length; i++) {
 
 			if (i >= pad_length)
