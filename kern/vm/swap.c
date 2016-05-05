0b8a8f17cb8ef4d41bce60e03394d00ee72771c6
Prasad Shivanna <prasadsjcecs@gmail.com>
2016-05-04 22:24:22 -0400

    Added header and C file for swapping, functions not implemented yet

---
+++ kern/vm/swap.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2008, 2009
+ *	The President and Fellows of Harvard College.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+ #include <types.h>
+ #include <lib.h>
+ #include <spl.h>
+ #include <vm.h>
+ #include <vfs.h>
+ #include <kern/fcntl.h>
+ #include <uio.h>
+ #include <kern/iovec.h>
+ #include <swap.h>
+ #include <proc.h>
+ #include <vnode.h>
+ #include <kern/stat.h>
+ #include <cpu.h>
+ #include <bitmap.h>
+
+static int swap_num_pages = 0;
+static bool is_first_swap = true;
+
+int
+swap_bootstrap(){
+  (void)swap_num_pages;
+  (void)is_first_swap;
+  return 0;
+}
+
+int
+page_swapout(int index){
+  (void)index;
+  return 0;
+}
+
+int
+page_swapin(struct page_table_entry* pte, paddr_t pa){
+  (void)pte;
+  (void)pa;
+  return 0;
+}
+
+int
+free_swap_index(int index){
+  (void)index;
+  return 0;
+}
